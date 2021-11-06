1846167dcf1d463ee9baf361228cdb8a9f846c4a
hadikhah <ma.hadikhah@raahin.com>
2021-11-06 15:10:57 +0330

    initial commit

---
+++ src/Providers/HumanResourceServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Raahin\HumanResource\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class HumanResourceServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
